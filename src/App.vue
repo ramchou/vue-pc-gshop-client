40ee7249e2c81c99db531d78b68a58b99b252b4e
ramchou <jorouor@163.com>
2020-06-05 14:11:56 +0800

    update routes.js

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,18 @@
 
     <!-- 一级路由组件显示区域 -->
     <router-view></router-view>
-    
-    <Footer />
 
+    <Footer />
   </div>
 </template>
 
 <script>
-import Header from './components/Header'
-import Footer from './components/Footer'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 export default {
   name: "App",
-  // 据不注册（只有当前组件可用）
-  components:{
+  // 局部注册（只有当前组件可用）
+  components: {
     Header,
     Footer
   }
--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import routes from './routes'
 Vue.use(VueRouter)
 
 
-import Home from '../pages/Home'
-import Search from '../pages/Search'
-import Register from '../pages/Register'
-import Login from '../pages/Login'
+
 export default new VueRouter({
     mode:'history',
-    routes: [
-        {
-            path: '/',
-            component: Home
-        },
-        {
-            path: '/search',
-            component: Search
-        },
-        {
-            path: '/register',
-            component: Register
-        },
-        {
-            path: '/login',
-            component: Login
-        }
-    ]
+    routes
 })
